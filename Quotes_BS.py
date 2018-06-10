0c583aa5c0c7d4d846c8e5093a9ddd47d710682e
milchoH <milcho92@gmail.com>
2018-06-10 22:33:16 +0300

    Quotes_BS.py soup game

--- Quotes_BS.py
+++ Quotes_BS.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
 
 all_quotes = []
 base_url = "http://quotes.toscrape.com"
 url = "/page/1"
 
 while url:
-
     res = requests.get(f"{base_url}{url}")
     print(f"Now scrapping {base_url}{url}...")
     soup = BeautifulSoup(res.text, "html.parser")
@@ -21,6 +21,40 @@ while url:
 
     next_btn = soup.find(class_="next")
     url = next_btn.find("a")["href"] if next_btn else None
-    sleep(2)
+    #sleep(2)
 
-print(all_quotes)
+
+def start_game():
+    quote = choice(all_quotes)
+    remaining_guesses = 4
+    print("Here's a quote: " )
+    print(quote["text"])
+    print(quote["author"])
+    guess = ''
+    while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+        guess = input(f"Who said this quote? Guesses remainig: {remaining_guesses}")
+        if guess.upper() == quote["author"].upper():
+            print("NICEEEE")
+            break
+        remaining_guesses -= 1
+        if remaining_guesses == 3:
+            res = requests.get(f"{base_url}{quote['bio-link']}")
+            soup = BeautifulSoup(res.text, "html.parser")
+            birth_date = soup.find(class_="author-born-date").get_text()
+            birth_place = soup.find(class_="author-born-location").get_text()
+            print(f"Here's a hint: \nThe author was born on {birth_date} {birth_place}")
+        elif remaining_guesses == 2:
+            print(f"Here's a hint: \nThe author's first name starts with {quote['author'][0]}")
+        elif remaining_guesses == 1:
+            last_initial = quote["author"].split(" ")[1][0]
+            print(f"Here's a hint: \nThe author's first name starts with {last_initial}")
+        else:
+            print("You lose N00b!!! \nThe answer was {quote['author']}")
+
+    again = ''
+    while again.lower() not in ('y','yes', 'n', 'no'):
+        again = input ("Would you like to play again (y/n)?")
+    if again.lower() in ('yes', 'y'):
+        return start_game()
+    else:
+        print("OK BYE BYE!")
