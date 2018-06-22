ea0d9d4448a26dae228ba806794896c23c4d02a8
milchoH <milcho92@gmail.com>
2018-06-22 23:52:04 +0300

    Stack implementation

---
+++ stack_array.py
@@ -0,0 +1,37 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return self.stack ==[]
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        data = self.stack[-1]
+        del self.stack[-1]
+        return data
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size_stack(self):
+        return len(self.stack)
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+stack.push(6)
+
+print(stack.size_stack())
+print("popped: ", stack.pop())
+print("popped: ", stack.pop())
+print(stack.size_stack())
+print("Peek ", stack.peek())
+print(stack.size_stack())
